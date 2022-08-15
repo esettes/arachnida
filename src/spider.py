b32a43b9c921dcb5749f1b988037db12e693dc8d
esettes <ioanaroxana.st@gmail.com>
2022-08-15 22:15:04 +0200

    add gitignore

--- src/spider.py
+++ src/spider.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python3.9
 import requests
 from bs4 import BeautifulSoup
-
+import os
 
 def	main():
 	URL = "https://realpython.github.io/fake-jobs/"
 	page = requests.get(URL)
 
 	soup = BeautifulSoup(page.content, "html.parser")
-	results = soup.find(id="ResultsContainer")
+	results = soup.findAll('img')
+	os.mkdir('img_folder')
 	print(results.prettify())
+
+	r = requests.get(URL).content
+	with open("myfile.txt", "wb") as f:
+		f.write(r)
 
 if __name__ == '__main__':
 	main()
