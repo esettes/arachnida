3bf949b3859d3e2a2e9c0f898889f5b1f6dd59e7
esettes <ioanaroxana.st@gmail.com>
2022-08-16 02:19:07 +0100

    add photon OS as docker image

--- src/spider.py
+++ src/spider.py
@@ -1,7 +1,8 @@
-#!/usr/bin/python3.9
+#!/usr/bin/python3
 import requests
 from bs4 import BeautifulSoup
 import os
+import bcolors as col
 
 def	main():
 	URL = "https://realpython.github.io/fake-jobs/"
@@ -9,7 +10,8 @@ def	main():
 
 	soup = BeautifulSoup(page.content, "html.parser")
 	results = soup.findAll('img')
-	os.mkdir('img_folder')
+	#os.mkdir('img_folder')
+	print(col.SPIDER + col.YELLOW +  "HooOOOOoOOOOoOoooOO")
 	print(results.prettify())
 
 	r = requests.get(URL).content
